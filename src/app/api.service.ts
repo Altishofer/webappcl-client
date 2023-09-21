d5bd1b76d0fc93b138b675da0ae62c0d3eaa2628
Patrick Widmer <patrickwidmer@gmx.ch>
2023-09-21 17:07:46 +0200

    added an api call to get some facts

---
+++ src/app/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  apiUrl = "https://catfact.ninja/fact";
+
+  constructor(private http: HttpClient) { }
+
+  freeFact(): Observable<any[]> {
+    return this.http.get<any>(`${this.apiUrl}`);
+  }
+}
+
+
+// API key: AIzaSyBN-xTXQR38tqTq6DoWNfFlwzduyYv3WUI
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from "./api.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-app';
+
+  constructor(private service: ApiService) {}
+
+  getFact() {
+    this.service.freeFact().subscribe((results) => {
+      console.log('Pineapples have been received - Result:\n', results);
+    })
+  }
+
+  ngOnInit(): void {
+  }
 }
