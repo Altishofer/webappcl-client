881f3230a5afaefe8bc01b0486534e23b375ce46
Chopstew <patrickwidmer@gmx.ch>
2023-10-04 14:12:35 +0200

    added link to logo

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from "@shared/shared.module";
+import { HomeRoutingModule } from "@modules/home/home-routing.module";
 
-import {SharedModule} from "@shared/shared.module";
 import { GameComponent } from '@modules/home/pages/game/game.component';
 import { SandrinComponent } from './pages/sandrin/sandrin.component';
-import {HomeRoutingModule} from "@modules/home/home-routing.module";
-import {HeaderComponent} from "@layout/header/header.component";
+import { HeaderComponent } from "@layout/header/header.component";
 
 @NgModule({
   declarations: [
