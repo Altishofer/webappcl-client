843bb575797ee68a659bad889a58955add6a90f4
Patrick Widmer <patrickwidmer@gmx.ch>
2023-10-20 13:41:34 +0200

    added button to add fields

--- src/app/modules/player/pages/word-calc/word-calc.component.ts
+++ src/app/modules/player/pages/word-calc/word-calc.component.ts
@@ -1,15 +1,26 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-word-calc',
   templateUrl: './word-calc.component.html',
   styleUrls: ['./word-calc.component.css']
 })
-export class WordCalcComponent implements AfterViewInit{
+export class WordCalcComponent implements AfterViewInit {
+  enteredWords = [{subtract: false, word: ''}]
+
+  updateSubtract(position: number) {
+    console.log(position);
+  }
+  addField() {
+    this.enteredWords.push({subtract: false, word: ''})
+  }
+
+
+
   @ViewChild('calculatorSwitch', { read: ElementRef }) element: ElementRef | undefined;
 
-  plus = 'M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z';
-  minus = 'M 0 10 h 24 v 4 h -24 z'
+  private plus = 'M 11 4 L 11 11 L 4 11 L 4 13 L 11 13 L 11 20 L 13 20 L 13 13 L 20 13 L 20 11 L 13 11 L 13 4 Z';
+  private minus = 'M 4 11 h 16 v 2 h -16 z';
 
   ngAfterViewInit() {
     if (this.element) {
