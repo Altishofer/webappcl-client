63689cd5ff7eb6f1eebeeb35e7eed11f8c0a5ef1
Patrick Widmer <patrickwidmer@gmx.ch>
2023-10-24 23:36:07 +0200

    added working quiz preview

--- src/app/data/services/quiz.service.ts
+++ src/app/data/services/quiz.service.ts
@@ -22,15 +22,20 @@ export class QuizService {
     return this.http.get(`${this.baseUrl}/GetAllQuizzes`, { observe:'response', headers });
   }
 
+  getAllRoundsByQuiz(quizId: string): Observable<any> {
+    const headers: HttpHeaders = new HttpHeaders({
+      'Authorization': "Bearer " + this.cookieService.get("token"),
+      'Content-Type': 'application/json',
+    });
+    return this.http.get(`${this.baseUrl}/GetAllRoundsByQuiz?quizId=${quizId}`, {observe: 'response', headers});
+  }
 
-  getQuiz(quizId: number) {
+  getQuiz(quizId: string) {
     const headers = new HttpHeaders({
       'Authorization': "Bearer " + this.cookieService.get("token"),
       'Content-Type': 'application/json',
     });
 
-    this.http.get(`${this.baseUrl}/GetQuiz/${quizId}`, { headers }).subscribe((response: any) => { //Todo: Replace fixed ID with dynamic solution
-      console.log("got quizzes from request: " + response);
-    });
+    return this.http.get(`${this.baseUrl}/GetQuiz/${quizId}`, { observe: 'response', headers });
   }
 }
--- src/app/modules/host/pages/quiz-preview/quiz-preview.component.ts
+++ src/app/modules/host/pages/quiz-preview/quiz-preview.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {QuizService} from "@data/services/quiz.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Round} from "@data/interfaces/round.model";
+import {Quiz} from "@data/interfaces/quiz.model";
 
 @Component({
   selector: 'app-quiz-preview',
   templateUrl: './quiz-preview.component.html',
   styleUrls: ['./quiz-preview.component.css']
 })
-export class QuizPreviewComponent {
+export class QuizPreviewComponent implements OnInit{
+  allRounds: Round[] = [];
+  quizId!: string;
+  quiz!: Quiz;
+  errorMsg : string = '';
+  unexpectedErrorMsg : string = "An unexpected error occurred."
 
+  constructor(private quizService: QuizService, private router: Router, private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      this.quizId = params['quizId'];
+    });
+  }
+
+  getAllRoundsByQuiz(quizId: string) {
+    this.quizService.getAllRoundsByQuiz(quizId).subscribe((response: any): void => {
+      if ((response.status >= 200 && response.status < 300) || response.status == 304) {
+        this.allRounds = response.body;
+      } else {
+        this.errorMsg = this.unexpectedErrorMsg;
+      }
+    });
+  }
+
+  getQuiz(quizId: string) {
+    this.quizService.getQuiz(quizId).subscribe((response: any): void => {
+      if ((response.status >= 200 && response.status < 300) || response.status == 304) {
+        this.quiz = response.body["quizDto"];
+      } else {
+        this.errorMsg = this.unexpectedErrorMsg;
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.getQuiz(this.quizId);
+    this.getAllRoundsByQuiz(this.quizId);
+  }
 }
