e202a8a17d128d7af5a75a5aaad03f5e8079612f
sandrin <sandrin@hunkeler.ws>
2023-10-30 09:31:02 +0100

    resolved wrong token error

--- src/app/data/services/host.service.ts
+++ src/app/data/services/host.service.ts
@@ -80,7 +80,7 @@ export class HostService {
 
   getIntermediateResult(quizId : string, roundId:string) : Observable<any> {
     const headers = new HttpHeaders({
-      'Authorization': "Bearer " + this.cookieService.get("playerToken"),
+      'Authorization': "Bearer " + this.cookieService.get("hostToken"),
       'Content-Type': 'application/json',
     });
     return this.http.get(`${this.quizUrl}/IntermediateResult/${quizId}/${roundId}`, { observe:'response', headers  });
