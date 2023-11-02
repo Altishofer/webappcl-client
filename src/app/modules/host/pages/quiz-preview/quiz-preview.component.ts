a9c15a70b2cb4b1e06fdafb1a3c344945f42935c
sandrin <sandrin@hunkeler.ws>
2023-11-02 23:30:15 +0100

    redesign

--- src/app/modules/host/pages/quiz-preview/quiz-preview.component.ts
+++ src/app/modules/host/pages/quiz-preview/quiz-preview.component.ts
@@ -90,8 +90,6 @@ export class QuizPreviewComponent implements OnInit{
       this.forbiddenWordsForm.addControl(round.id, this.fb.array(lst));
       this.targetWordForm.addControl(round.id, this.createWordFormGroup(round.roundTarget, false));
     });
-    console.log('forbiddenWordsForm: ', this.forbiddenWordsForm.value);
-    console.log('targetWordForm: ', this.targetWordForm.value);
   }
 
   selectText(event: any) {
@@ -104,12 +102,10 @@ export class QuizPreviewComponent implements OnInit{
 
   addField(roundId:string, word:string = '', isSubtracted:boolean = false) : void {
     const formArray: FormArray<any> = this.forbiddenWordsForm.get(roundId) as FormArray;
-    console.log('AddForbiddenWord_Before', formArray.value);
     if (formArray){
       formArray.push(this.createWordFormGroup(word, isSubtracted));
     }
     this.addFieldDisabled = true;
-    console.log('AddForbiddenWord_After', formArray.value);
   }
 
 
@@ -141,19 +137,14 @@ export class QuizPreviewComponent implements OnInit{
   }
 
   removeField(roundId : string, index: number) : void {
-    console.log('deleteForbiddenWord', roundId, index);
     const formArray: FormArray<any> = this.forbiddenWordsForm.get(roundId) as FormArray;
-    console.log('deleteForbiddenWord', formArray.value);
     if (formArray){
       const control: AbstractControl<any, any> = formArray.at(index);
         if (control) {
           formArray.removeAt(index);
-          //formArray.updateValueAndValidity();
         }
       }
     this.addFieldDisabled = this.allIndexWordNonEmpty(roundId);
-    //this.cd.detectChanges();
-    console.log('RemoveForbiddenWord_After', formArray.value);
   }
 
   changes() : void {
@@ -161,8 +152,8 @@ export class QuizPreviewComponent implements OnInit{
   }
 
   addNewRound(){
-    let lst: AbstractControl<any, any>[] = [];
-    lst.push(this.createWordFormGroup('', false));
+    let lstForbWordControl: AbstractControl<any, any>[] = [];
+    lstForbWordControl.push(this.createWordFormGroup('ChangeThisWOrd', false));
 
     let maxId: number = 0;
     this.selectedQuizRounds.forEach((round: Round) => {
@@ -170,14 +161,15 @@ export class QuizPreviewComponent implements OnInit{
         maxId = Number(round.id);
       }
     });
-    this.forbiddenWordsForm.addControl(String(maxId+1), this.fb.array(lst));
-    let round : Round = {id : String(maxId+1), quizId : String(this.selectedQuizId), roundTarget : 'ChangeThisWord', forbiddenWords : ['']};
-    this.selectedQuizRounds.push(round);
+    this.forbiddenWordsForm.addControl(String(maxId+1), this.fb.array(lstForbWordControl));
+    this.targetWordForm.addControl(String(maxId+1), this.createWordFormGroup('ChangeThisWord', false));
+    // let round : Round = {id : String(maxId+1), quizId : String(this.selectedQuizId), roundTarget : 'ChangeThisWord', forbiddenWords : ['']};
+    // this.selectedQuizRounds.push(round);
   }
 
   removeRound(roundId: string) {
     this.forbiddenWordsForm.removeControl(roundId);
-    this.selectedQuizRounds.splice(this.selectedQuizRounds.findIndex((round: Round) => round.id === roundId), 1);
+    // this.selectedQuizRounds.splice(this.selectedQuizRounds.findIndex((round: Round) => round.id === roundId), 1);
   }
 
   trackByFn(index: any, item: any) {
@@ -208,23 +200,6 @@ export class QuizPreviewComponent implements OnInit{
       rounds : this.selectedQuizRounds
     }
 
-    //quiz = {
-    //  quizId : 11,
-    //  hostId : 5,
-    //  title : 'honolulu',
-    //  rounds : [{
-    //    "id": "7",
-    //    "quizId": "11",
-    //    "roundTarget": "dog",
-    //    "forbiddenWords": [
-    //      "cat",
-    //      "mouse",
-    //      "bottle"
-    //    ]
-    //  }]
-    //}
-
-    console.log(quiz);
     if (this.selectedQuizId == -1) {
       this._quizService.createQuiz(quiz).subscribe((response: any): void => {
         console.log("REST quiz: ", response.body);
